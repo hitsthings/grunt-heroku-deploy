a2ae2ca1555f729bffa2884915e8b84dcb2676fe
Jesse Ditson <jesse.ditson@gmail.com>
2013-02-12 14:23:05 -0800

    move lifting to lib file, add grunt 0.4.x compatibility

--- tasks/heroku-deploy.js
+++ tasks/heroku-deploy.js
@@ -6,67 +6,16 @@
  * Licensed under the MIT license.
  */
 
-var spawn = require('child_process').spawn;
-
-function pipeAll(proc) {
-  proc.stdout.pipe(process.stdout);
-  proc.stderr.pipe(process.stderr);
-  return proc;
-}
-
-function allOutput(proc, next) {
-  var out = '';
-  proc.stdout.on('data', function(data) { out += data; });
-  proc.stdout.on('end', function() {
-    next(null, out);
-  });
-}
-
-function doDeploy(originRef, deployRef, next) {
-  pipeAll(spawn('git', ['checkout', deployRef])).on('exit', function() {
-    pipeAll(spawn('git', ['merge', originRef])).on('exit', function() {
-      pipeAll(spawn('git', ['push'])).on('exit', function() {
-        pipeAll(spawn('git', ['checkout', originRef])).on('exit', function() {
-          next();
-        });
-      });
-    });
-  });
-}
-
-function getCurrentBranch(next) {
-  allOutput(spawn('git', ['branch']), function(err, out) {
-    if (err) {
-      return next(err);
-    }
-
-    var newline, current, branch;
-    if (out[0] === '*') {
-      current = 0;
-    } else {
-      current = out.indexOf('\n*') + 1;
-      if (!current) {
-        return next(new Error("Current branch could not be determined."));
-      }
-    }
-
-    newline = out.indexOf('\n', current);
-    branch = out.substring(current + 2, ~newline ?  newline : undefined);
-
-    next(null, branch === '(no branch)' ? null : branch);
-  });
-}
-
-function getCurrentCommitHash(next) {
-  allOutput(spawn('git', ['log', '-1', '--format=format:"%H"']), function(err, out) {
-    next(err, out && out.replace(/\n|\r/g, ''));
-  });  
-}
-
 module.exports = function(grunt) {
 
   // Please see the grunt documentation for more information regarding task and
   // helper creation: https://github.com/cowboy/grunt/blob/master/docs/toc.md
+  
+  // ==========================================================================
+  // INIT
+  // ==========================================================================
+  
+  var herokuDeploy = require('./lib/heroku-deploy').init(grunt)
 
   // ==========================================================================
   // TASKS
@@ -75,8 +24,7 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('heroku-deploy', 'Switch to the deploy branch, merge your starting location, push, and switch back', function() {
     var next = this.async();
     grunt.log.write(
-      grunt.helper(
-        'heroku-deploy',
+      herokuDeploy['deploy'](
         grunt.config(['heroku-deploy', this.target, 'deployBranch']),
         function(err) {
           if (err) {
@@ -86,36 +34,6 @@ module.exports = function(grunt) {
         }
       )
     );
-  });
-
-  // ==========================================================================
-  // HELPERS
-  // ==========================================================================
-
-  grunt.registerHelper('heroku-deploy', function(deployBranch, next) {
-    deployBranch = deployBranch || 'deploy';
-
-    getCurrentBranch(function(err, branch) {
-      if (err) {
-        return next(err);
-      }
-
-      if (!branch) {
-        console.log('No current branch');
-
-        getCurrentCommitHash(function(err, csid) {
-          if (err) {
-            return next(err);
-          }
-
-          console.log('Using ' + csid + ' as ref to merge.');
-          doDeploy(csid, deployBranch, next);
-        });
-      } else {
-        console.log('Current branch is ' + branch);
-        doDeploy(branch, deployBranch, next);
-      }
-    });
   });
 
 };
